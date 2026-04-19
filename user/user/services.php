5e6a6841dc06423285f133952b119efbd8634fe0
Nyana Smart Services <nyanasmartservices@gmail.com>
2026-04-19 23:18:00 +0200

    Create services.php

---
+++ user/user/services.php
@@ -0,0 +1,14 @@
+<h2>Book Service</h2>
+
+<form method="POST" action="../booking.php">
+<input type="text" name="name" placeholder="Your Name"><br>
+
+<select name="service">
+<option>Irembo</option>
+<option>RRA</option>
+<option>Mituel</option>
+<option>Printing</option>
+</select><br>
+
+<button>Book</button>
+</form>
